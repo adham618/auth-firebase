1b754c9b16ee978b39ece8e184894adf3badb549
Adham Tarek <adhamtarek291@gmail.com>
2024-05-04 21:35:44 +0300

    add

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -11,13 +11,12 @@ test("should navigate to the about page", async ({ page }) => {
   await expect(page.locator("h1")).toContainText("About");
 });
 
-test("should navigate to the about page", async ({ page }) => {
-  // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
-  await page.goto("/");
-  // Find an element with the text 'About' and click on it
-  await page.click("text=About");
-  // The new URL should be "/about" (baseURL is used there)
-  await expect(page).toHaveURL("/about");
-  // The new page should contain an h1 with "About"
-  await expect(page.locator("h1")).toContainText("About");
+test("has title", async ({ page }) => {
+  await page.goto("/login");
+
+  // Expect page to have a title Login
+  await expect(page).toHaveTitle(/Login/);
+
+  // Found an element with the text "Log in with Google" and click it
+  await page.getByRole("button", { name: "Log in with Google" }).click();
 });
