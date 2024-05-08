5714560d43eaee8a84d2a34ad68fa7dbf99df376
Adham Tarek <adhamtarek291@gmail.com>
2024-05-09 01:32:32 +0300

    add

--- cypress/e2e/test_hello_world.cy.ts
+++ cypress/e2e/test_hello_world.cy.ts
@@ -1,16 +1,16 @@
 describe("Some Test", () => {
   it("Try login", () => {
-    // if (Cypress.env("NEXT_PUBLIC_CI_ENV") !== "true") {
-    //   cy.visit("/login");
-    //   const TEST_UID = Cypress.env("CYPRESS_TEST_UID");
-    //   cy.login(TEST_UID);
-    // }
+    if (Cypress.env("NEXT_PUBLIC_CI_ENV") !== "true") {
+      cy.visit("/login");
+      const TEST_UID = Cypress.env("CYPRESS_TEST_UID");
+      cy.login(TEST_UID);
+    }
     cy.callFirestore("add", "test_hello_world", { some: "value" });
 
     cy.visit("/");
-    // cy.location("pathname").should("eq", "/");
-    // if (Cypress.env("NEXT_PUBLIC_CI_ENV") !== "true") {
-    //   cy.logout();
-    // }
+    cy.location("pathname").should("eq", "/");
+    if (Cypress.env("NEXT_PUBLIC_CI_ENV") !== "true") {
+      cy.logout();
+    }
   });
 });
