f16c798048d8b2abeb8ac333b1b4e076e3a5de87
Adham Tarek <adhamtarek291@gmail.com>
2024-05-09 01:51:09 +0300

    add

--- cypress/e2e/test_hello_world.cy.ts
+++ cypress/e2e/test_hello_world.cy.ts
@@ -4,6 +4,8 @@ describe("Some Test", () => {
     const TEST_UID = Cypress.env("CYPRESS_TEST_UID");
     cy.login(TEST_UID);
     cy.wait(10000);
+    // click button log in with google
+    cy.get("button").contains("Log in with Google").click();
     cy.logout();
   });
 });
