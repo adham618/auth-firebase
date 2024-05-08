1ffdee40245692b92d38c10b6e5c487f0041cfd8
Adham Tarek <adhamtarek291@gmail.com>
2024-05-08 05:46:01 +0300

    add

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,22 +6,12 @@ require("dotenv").config({ path: ".env.local" });
 export default defineConfig({
   projectId: "7ymsq5",
   experimentalModifyObstructiveThirdPartyCode: true,
+  chromeWebSecurity: false,
   env: {
     CYPRESS_TEST_EMAIL: process.env.CYPRESS_TEST_EMAIL,
     CYPRESS_TEST_PASSWORD: process.env.CYPRESS_TEST_PASSWORD,
   },
   e2e: {
     baseUrl: "http://localhost:3000",
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-      on("before:browser:launch", (browser, launchOptions) => {
-        console.log(launchOptions.args);
-        let removeFlags = ["--enable-automation"];
-        launchOptions.args = launchOptions.args.filter(
-          (value) => !removeFlags.includes(value)
-        );
-        return launchOptions;
-      });
-    },
   },
 });
