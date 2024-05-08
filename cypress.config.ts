9a26649429558b11c42087de31a056d64231e09e
Adham Tarek <adhamtarek291@gmail.com>
2024-05-08 04:58:37 +0300

    fix eror

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,7 +5,6 @@ require("dotenv").config({ path: ".env.local" });
 
 export default defineConfig({
   experimentalModifyObstructiveThirdPartyCode: true,
-  chromeWebSecurity: false,
   env: {
     CYPRESS_TEST_EMAIL: process.env.CYPRESS_TEST_EMAIL,
     CYPRESS_TEST_PASSWORD: process.env.CYPRESS_TEST_PASSWORD,
