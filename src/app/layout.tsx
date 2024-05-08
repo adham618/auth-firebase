938959621ad78a189941cfa8f7b5e0561b40643c
Adham Tarek <adhamtarek291@gmail.com>
2024-05-09 00:28:59 +0300

    add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,25 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   const tokens = await getTokens(cookies(), authConfig);
-  const user = tokens ? toUser(tokens) : null;
+  // const user = tokens ? toUser(tokens) : null;
+  const user =
+    process.env.NEXT_PUBLIC_CI_ENV === "true"
+      ? {
+          uid: "tPaNDwNmyKRZOxmVtoq3JKBTBPt2",
+          email: "e2e-test-only-for-ci@gmail.com",
+          displayName: "e2e-test-only-for-ci",
+          photoURL:
+            "https://lh3.googleusercontent.com/a/ACg8ocLzsKyzjg-LKNZTrXrg6GsiERArNHQlhSXEW9FIq3yKHhAhEg=s96-c",
+          phoneNumber: null,
+          emailVerified: true,
+          providerId: "google.com",
+          customClaims: {},
+        }
+      : tokens
+      ? toUser(tokens)
+      : null;
+
+  console.log("user", user);
 
   return (
     <html lang="en">
