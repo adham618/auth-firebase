63ccd17dc30a710804434d08647d1a61b0760d17
Adham Tarek <adhamtarek291@gmail.com>
2024-05-08 09:22:38 +0300

    add

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -2,17 +2,14 @@ describe("Google", function () {
   beforeEach(function () {
     cy.visit("/login");
     cy.login(Cypress.env("CYPRESS_TEST_UID"));
-    cy.wait(4000);
   });
 
   it("should be able to visit home page", function () {
     cy.visit("/");
-    cy.wait(4000);
     cy.location("pathname").should("eq", "/");
   });
 
   it("should be able to visit profile page and logout", function () {
-    cy.wait(4000);
     cy.visit("/");
     cy.get("a").contains("Go to profile page").click();
     cy.location("pathname").should("eq", "/profile");
