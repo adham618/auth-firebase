d6e23f78db928a61d2279f6527f3ca6653086f02
Adham Tarek <adhamtarek291@gmail.com>
2024-05-08 04:11:58 +0300

    add

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -7,34 +7,34 @@ Cypress.Commands.add("login", () => {
   // Verify if the "Log in with Google" button is visible
   cy.get("button").contains("Log in with Google").should("be.visible").click();
 
-  // Handle Google authentication
-  cy.origin("https://accounts.google.com", () => {
-    // Handle uncaught exceptions gracefully
-    cy.on("uncaught:exception", (err, runnable) => {
-      console.error("Google Login -> uncaught:exception", err);
-      // Ignore error and continue test execution
-      return false;
-    });
+  // // Handle Google authentication
+  // cy.origin("https://accounts.google.com", () => {
+  //   // Handle uncaught exceptions gracefully
+  //   cy.on("uncaught:exception", (err, runnable) => {
+  //     console.error("Google Login -> uncaught:exception", err);
+  //     // Ignore error and continue test execution
+  //     return false;
+  //   });
 
-    // Enter email
-    cy.url()
-      .should("contain", "accounts.google.com")
-      .get('input[type="email"]')
-      .type(Cypress.env("CYPRESS_TEST_EMAIL"))
-      .type("{enter}")
-      .wait(3000);
+  //   // Enter email
+  //   cy.url()
+  //     .should("contain", "accounts.google.com")
+  //     .get('input[type="email"]')
+  //     .type(Cypress.env("CYPRESS_TEST_EMAIL"))
+  //     .type("{enter}")
+  //     .wait(3000);
 
-    // Enter password
-    cy.url()
-      .should("contain", "accounts.google.com")
-      .get('input[type="password"]')
-      .type(Cypress.env("CYPRESS_TEST_PASSWORD"))
-      .type("{enter}");
-  });
+  //   // Enter password
+  //   cy.url()
+  //     .should("contain", "accounts.google.com")
+  //     .get('input[type="password"]')
+  //     .type(Cypress.env("CYPRESS_TEST_PASSWORD"))
+  //     .type("{enter}");
+  // });
 
-  // Verify successful login by checking URL redirection
-  cy.url().should("not.contain", "accounts.google.com");
+  // // Verify successful login by checking URL redirection
+  // cy.url().should("not.contain", "accounts.google.com");
 
-  // Wait for the page to fully redirect
-  cy.wait(10000);
+  // // Wait for the page to fully redirect
+  // cy.wait(10000);
 });
