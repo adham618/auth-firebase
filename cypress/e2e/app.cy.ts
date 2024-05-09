8ac5b4b8a2f172774380130c30f1c5af414bb772
Adham Tarek <adhamtarek291@gmail.com>
2024-05-09 03:05:38 +0300

    add

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -13,13 +13,12 @@ describe("Some Test", () => {
     // check if user is logged in
     cy.contains("You are logged in").should("be.visible");
     // click button to go to profile
-    cy.get("button").contains("Go to profile page").click();
+    cy.get("a").contains("Go to profile page").click();
     // check pathname is "/profile"
     cy.location("pathname").should("eq", "/profile");
     // check if user is logged in
     cy.contains("You are logged in").should("be.visible");
     // click button to logout
     cy.get("button").contains("Logout").click();
-    // cy.location("pathname").should("eq", "/");
   });
 });
