f8253d0c12076383891c1f689525a9af0af27378
Adham Tarek <adhamtarek291@gmail.com>
2024-05-09 03:01:43 +0300

    edit tests

---
+++ cypress/e2e/app.cy.ts
@@ -0,0 +1,25 @@
+describe("Some Test", () => {
+  beforeEach(() => {
+    if (!Cypress.env("NEXT_PUBLIC_CI_ENV")) {
+      cy.visit("/login");
+      const TEST_UID = Cypress.env("CYPRESS_TEST_UID");
+      cy.login(TEST_UID);
+    }
+  });
+  it("Should be able to login and redirect to home page", () => {
+    cy.visit("/");
+    // check pathname is "/"
+    cy.location("pathname").should("eq", "/");
+    // check if user is logged in
+    cy.contains("You are logged in").should("be.visible");
+    // click button to go to profile
+    cy.get("button").contains("Go to profile page").click();
+    // check pathname is "/profile"
+    cy.location("pathname").should("eq", "/profile");
+    // check if user is logged in
+    cy.contains("You are logged in").should("be.visible");
+    // click button to logout
+    cy.get("button").contains("Logout").click();
+    // cy.location("pathname").should("eq", "/");
+  });
+});
